5a80217e324347d5a914fdc536cf27cad009f58f
Pascal Stücker <131346899+pstuecker@users.noreply.github.com>
2023-11-13 14:45:07 +0100

    Set DPI awareness for chromium_subp to "System Aware" (#42)

--- native/chromium_subp/src/bin/chromium_subp.rs
+++ native/chromium_subp/src/bin/chromium_subp.rs
@@ -20,6 +20,18 @@ use std::os::raw::c_int;
 use std::ptr::null_mut;
 
 fn main() {
+    // Disable High DPI per Monitor support.
+    //   By default Chromium enables the "PerMonitorV2" strategy for high DPI awareness. However SWT does not yet support
+    //   "PerMonitorV2", but only "System" DPI awareness. Mismatching DPI awareness between the render process (chromium)
+    //   and the host process (swt/java) causes strange visual artifacts.
+    //
+    //   Thus set "System" DPI awareness here, to override Chromiums default to match SWT.
+    unsafe {
+        winapi::um::winuser::SetProcessDpiAwarenessContext(
+            winapi::shared::windef::DPI_AWARENESS_CONTEXT_SYSTEM_AWARE,
+        );
+    }
+
     let main_args = chromium::utils::prepare_args();
     let mut app = app::App::new();
     let exit_code: c_int =
