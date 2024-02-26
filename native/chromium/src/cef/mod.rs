8502572e495a07e2785c7d26f34ca8677bf7d953
Pascal Stücker <stuecker.pascal@scheidt-bachmann-st.de>
2024-02-26 08:21:42 +0100

    Adapt according to jni changes

--- native/chromium/src/cef/mod.rs
+++ native/chromium/src/cef/mod.rs
@@ -16,6 +16,7 @@ use crate::ToJava;
 use jni::objects::GlobalRef;
 use jni::objects::JObject;
 use chromium_jni_utils::jni_unwrap;
+use std::ops::DerefMut;
 pub mod win;
 pub use self::win::_cef_window_info_t;
 pub use self::win::_cef_main_args_t;
