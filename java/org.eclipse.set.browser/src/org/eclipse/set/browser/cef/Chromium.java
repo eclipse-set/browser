75310d768f846d8508d1af7807752d51c70326a0
Marius Heine <m.heine@geprog.com>
2026-06-17 01:50:28 +0200

    remove unused import

--- java/org.eclipse.set.browser/src/org/eclipse/set/browser/cef/Chromium.java
+++ java/org.eclipse.set.browser/src/org/eclipse/set/browser/cef/Chromium.java
@@ -71,7 +71,6 @@ import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.internal.C;
 import org.eclipse.swt.internal.Callback;
 import org.eclipse.swt.internal.DPIUtil;
-import org.eclipse.swt.internal.Win32DPIUtils;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Display;
